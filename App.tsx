ab798a4dd4f6915911d91e9e44c63e5568c55c7d
FELIPE FERREIRA DE FRANÇA <1051392421013@fatec.corp>
2026-09-21 11:05:53 -0300

    feat: Criadas as estruturas base do projeto

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
+import Home from './screens/HomeScreen';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+  <View style={styles.container}>
+    <Header />
+    <Home />
+    
+  </View>
+
+ )
 }
+
+
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ screens/AddBookScreen.tsx
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Home() {
+    return (
+        
+        <View style={styles.container}>
+            <View style={styles.banner}>
+                <Text style={styles.title}>
+                    Adicione livros
+                </Text>
+                <Text style={styles.description}>
+                </Text>
+            </View>
+        </View>
+ );
+}
+const styles = StyleSheet.create({
+ container: {
+ flex: 1,
+ },
+ header: {
+ padding: 20,
+ },
+ logo: {
+ fontSize: 24,
+ fontWeight: 'bold',
+ },
+ banner: {
+ padding: 20,
+ },
+ title: {
+ fontSize: 28,
+ fontWeight: 'bold',
+ },
+ description: {
+ fontSize: 16,
+ marginTop: 10,
+ padding: 20,
+ 
+ },
+});
