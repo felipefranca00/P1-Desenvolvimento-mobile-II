391aa8eb104396a14800f072478aefcabef0f6de
FELIPE FERREIRA DE FRANÇA <1051392421013@fatec.corp>
2026-09-21 09:32:40 -0300

    primeiro commit

---
+++ screens/Home.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native';
+
+
+export default function Home() {
+    return (
+
+    <View>
+        <View>
+            <Text>TechStore</Text>
+        </View>
+        <View>
+            <Text>Tecnologia para o seu dia</Text>
+            <Text>Encontre os melhores produtos para sua vida digital.</Text>
+        </View>
+    </View>
+
+);
+
+}
